4e4f4fdaefa4389fa21dbc160ea5689befee1928
marcosvillarreal <marcosevillarreal@gmail.com>
2018-04-11 15:24:46 -0300

    m:comercio

---
+++ Proyectos/ceo/SCRIP_SQL/Limpieza.sql
@@ -0,0 +1,48 @@
+use ceo
+go
+delete from maopera
+delete from ncuerfac
+delete from cuerruta
+delete from anmovstock
+delete from ancuerfac
+delete from tablaasi
+delete from cuervari
+delete from ncuervari
+delete from tablaimp
+delete from antablaimp
+delete from cabefac
+delete from ancabefac
+delete from fletecarga
+delete from nmaopera
+delete from ncabefac
+delete from afenpedido
+delete from renmaope
+delete from cabeasi
+delete from ancabeasi
+delete from cbioprecio
+delete from movctacte
+delete from pedipda
+delete from visita
+delete from prodctacon
+delete from ctactectacon
+delete from afectacte
+delete from movcaja
+delete from cuerord
+delete from cabedeta
+delete from caberuta
+delete from alertas
+delete from existenc
+delete from cuercpra
+delete from fleteplanilla
+delete from cuervariord
+delete from cuerdeta
+delete from anmaopera
+delete from fleteren
+delete from renflete
+delete from movcomodato
+delete from cabecpra
+delete from cuervaricpra
+
+
+execute actualizarid 1
+select * from keysid  order by nextid desc
