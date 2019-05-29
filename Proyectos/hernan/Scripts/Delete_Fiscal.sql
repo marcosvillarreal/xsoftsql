246f10e26449488e2559693e439d6b7f7683f613
marcosvillarreal <marcosevillarreal@gmail.com>
2019-05-29 20:17:08 -0300

    m:leon
    version anterior del resumen stock

--- Proyectos/hernan/Scripts/Delete_Fiscal.sql
+++ Proyectos/hernan/Scripts/Delete_Fiscal.sql
@@ -3,33 +3,33 @@ go
 Delete from dcuervari  Where dcuervari.idmaopera in 
 		(select dmaopera.id from dmaopera 
 		left join dcabefac on dmaopera.id = dcabefac.idmaopera
-		where  dcabefac.fecha < '20180801'	)
+		where  dcabefac.fecha < '20190501'	)
 			go
 Delete from dcabedeta Where dcabedeta.idmaopera in 
 		(select dmaopera.id from dmaopera 
 		left join dcabefac on dmaopera.id = dcabefac.idmaopera
-		where  dcabefac.fecha < '20180801'	)
+		where  dcabefac.fecha < '20190501'	)
 go
 Delete from dcuerfac Where dcuerfac.idmaopera in 
 		(select dmaopera.id from dmaopera 
 		left join dcabefac on dmaopera.id = dcabefac.idmaopera
-		where  dcabefac.fecha < '20180801'	)
+		where  dcabefac.fecha < '20190501'	)
 go
 Delete from dpagos Where dpagos.idmaopera in 
 		(select dmaopera.id from dmaopera 
 		left join dcabefac on dmaopera.id = dcabefac.idmaopera
-		where  dcabefac.fecha < '20180801'	)
+		where  dcabefac.fecha < '20190501'	)
 go
 Delete from dtablaasi Where dtablaasi.idmaopera in 
 		(select dmaopera.id from dmaopera 
 		left join dcabefac on dmaopera.id = dcabefac.idmaopera
-		where  dcabefac.fecha < '20180801'	)
+		where  dcabefac.fecha < '20190501'	)
 go
 Delete from dmaopera Where dmaopera.id in 
 		(select dmaopera.id from dmaopera 
 		left join dcabefac on dmaopera.id = dcabefac.idmaopera
-		where  dcabefac.fecha < '20180801'	)
+		where  dcabefac.fecha < '20190501'	)
 go
-Delete from dcabefac Where dcabefac.fecha < '20180801'
+Delete from dcabefac Where dcabefac.fecha < '20190501'
 
 go
