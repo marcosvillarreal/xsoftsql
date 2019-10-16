9d0e62bd9ceba3b7049bf575ffef606c163b3d10
marcosvillarreal <marcosevillarreal@gmail.com>
2019-10-16 11:08:48 -0300

    m:distribuidora

--- Proyectos/distribuidora/Docs/Truncar_2012.sql
+++ Proyectos/distribuidora/Docs/Truncar_2012.sql
@@ -1,12 +1,12 @@
 use urquiza
 go
-sp_helpdb urquiza
+--sp_helpdb urquiza
 ---- Antes de truncar el log cambiamos el modelo de recuperación a SIMPLE.
 --ALTER DATABASE urquiza
 --SET RECOVERY SIMPLE;
 --GO
 ----Reducimos el log de transacciones a  1 MB.
---DBCC SHRINKFILE(gestion_log, 1);
+--DBCC SHRINKFILE(urquiza_log, 1);
 --GO
 ---- Cambiamos nuevamente el modelo de recuperación a Completo.
 --ALTER DATABASE urquiza
