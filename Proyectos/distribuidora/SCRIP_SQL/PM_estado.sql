884fd05a2bca6874756f35e38303894da7355026
marcosvillarreal <marcosevillarreal@gmail.com>
2019-11-20 10:57:56 -0300

    m:distribuidora
    cot

---
+++ Proyectos/distribuidora/SCRIP_SQL/PM_estado.sql
@@ -0,0 +1,6 @@
+use pm
+go
+select * from empresa where id = 13
+select top 1 fecupdate from vendedor where idempresa = 13
+select top 1 fecupdate from articulos where idempresa = 13
+select top 1 fecupdate from movctacte where idempresa = 13
