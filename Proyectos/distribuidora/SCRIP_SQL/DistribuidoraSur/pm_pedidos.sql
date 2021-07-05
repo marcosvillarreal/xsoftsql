78373dd480a3a3464b966f94f5bde581f9e79feb
marcosvillarreal <marcosevillarreal@gmail.com>
2021-07-05 16:35:35 -0300

    m:distribuidora

--- Proyectos/distribuidora/SCRIP_SQL/DistribuidoraSur/pm_pedidos.sql
+++ Proyectos/distribuidora/SCRIP_SQL/DistribuidoraSur/pm_pedidos.sql
@@ -1,11 +1,14 @@
 use pm
 go
---update cabpedido set loteexportacion='' where id = 922029
 
-select clientes.nombre,cabpedido.* from cabpedido 
+
+select clientes.nombre,cabpedido.fecha,cabpedido.horain,cabpedido.total from cabpedido 
 left join clientes on cabpedido.idcliente = clientes.numero and cabpedido.idempresa =clientes.idempresa
-where cabpedido.idempresa = 5 and vendedor = 9 --and idcliente = 1007--and total= '0' --and loteexportacion='-20210427'
+where cabpedido.idempresa = 5 and vendedor = 7 
+and  idcliente  in (284)
+
+--and total= '0' --and loteexportacion='-20210427'
 order by fecha desc, horain desc
 --update cuepedido set cantidad = 0 where id = 21013254
-select * from cuepedido where idcabpedido in (925429)
-select * from articulos where numero = 31042 and idempresa = 5
+--select * from cuepedido where idcabpedido in (957543)
+--select * from articulos where numero = 31042 and idempresa = 5
