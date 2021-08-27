2bc9d8c6b3247996b02f16f7c78513044de7343e
marcosvillarreal <marcosevillarreal@gmail.com>
2021-08-27 11:47:08 -0300

    m:distribuidora
    gs1

--- Proyectos/distribuidora/SCRIP_SQL/DistribuidoraSur/pm_pedidos.sql
+++ Proyectos/distribuidora/SCRIP_SQL/DistribuidoraSur/pm_pedidos.sql
@@ -7,12 +7,13 @@ go
 select clientes.nombre,cabpedido.* from cabpedido 
 left join clientes on cabpedido.idcliente = clientes.numero and cabpedido.idempresa =clientes.idempresa
 where cabpedido.idempresa = 5 
---and vendedor = 9
---and  idcliente  in (765)
---and total= '0' 
+and vendedor = 9
+and  idcliente  in (107)
+and (total <> '0' and total<>'')
 and isnull(loteexportacion,'')=''
 --AND ISNULL(TIPODEPEDIDO,'0')<>'0'
 order by fecha desc, horain desc
---update cuepedido set cantidad = 0 where id = 21013254
---select * from cuepedido where idcabpedido in (960594)
+update cuepedido set cantidad = 0 where id = 22566766
+select * from cuepedido where idcabpedido in (986873,
+986872)
 --select * from articulos where numero = 20639 and idempresa = 5
