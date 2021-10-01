adb6991881965acf3e22a26a19208f1c53032b28
marcosvillarreal <marcosevillarreal@gmail.com>
2021-10-01 10:24:48 -0300

    m:robotcae

--- Proyectos/distribuidora/SCRIP_SQL/DistribuidoraSur/pm_pedidos.sql
+++ Proyectos/distribuidora/SCRIP_SQL/DistribuidoraSur/pm_pedidos.sql
@@ -3,17 +3,17 @@ go
 
 --update cabpedido set tipodepedido = 1 , remito = 2 where idempresa = 5 and vendedor = 5 and left(ltrim(observaciones),1)='R'
 --update cabpedido set  tipodepedido=1, remito = 0  where idempresa = 5 and vendedor = 9 and isnull(remito,0)=0
---update cabpedido set perceiibb = 1533.95, total=10896.60 where id = 958910
+update cabpedido set loteexportacion = '' where id = 1006216
 select clientes.nombre,cabpedido.* from cabpedido 
 left join clientes on cabpedido.idcliente = clientes.numero and cabpedido.idempresa =clientes.idempresa
 where cabpedido.idempresa = 5 
-and vendedor = 9
-and  idcliente  in (107)
+--and vendedor = 8
+and  idcliente  in (729)
 and (total <> '0' and total<>'')
-and isnull(loteexportacion,'')=''
+--and isnull(loteexportacion,'')=''
 --AND ISNULL(TIPODEPEDIDO,'0')<>'0'
 order by fecha desc, horain desc
-update cuepedido set cantidad = 0 where id = 22566766
+--update cuepedido set cantidad = 0 where id = 22566766
 select * from cuepedido where idcabpedido in (986873,
 986872)
 --select * from articulos where numero = 20639 and idempresa = 5
