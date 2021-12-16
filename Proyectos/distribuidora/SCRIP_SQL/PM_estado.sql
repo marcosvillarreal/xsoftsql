71271abc8743a385ceaef565d32297c8b20530d6
marcosvillarreal <marcosevillarreal@gmail.com>
2021-12-16 14:03:29 -0300

    m:hernan

--- Proyectos/distribuidora/SCRIP_SQL/PM_estado.sql
+++ Proyectos/distribuidora/SCRIP_SQL/PM_estado.sql
@@ -1,15 +1,15 @@
 use pm
 go
-select * from empresa where id = 6
+--select * from empresa where id = 6
 select top 1 fecupdate,idempresa from vendedor --where idempresa = 6
-order by fecupdate desc
-select top 1 fecupdate,idempresa from articulos --where idempresa = 6 
 order by fecupdate desc
 select top 1 fecupdate,idempresa from secciones --where idempresa = 6 
 order by fecupdate desc
-select top 1 fecupdate,idempresa from codbarras --where idempresa = 6 
-order by fecupdate desc
-select top 1 fecupdate,idempresa from clientes --where idempresa = 6 
-order by fecupdate desc
+--select top 1 fecupdate,idempresa from articulos --where idempresa = 6 
+--order by fecupdate desc
+--select top 1 fecupdate,idempresa from codbarras --where idempresa = 3
+--order by fecupdate desc
+--select top 1 fecupdate,idempresa from clientes --where idempresa = 6 
+--order by fecupdate desc
 --select top 1 fecupdate,idempresa from movctacte --where idempresa = 13 
 --order by fecupdate  desc
