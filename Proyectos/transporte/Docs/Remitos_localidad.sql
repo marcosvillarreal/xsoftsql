169ce3096402534ff26520453809c081617f62da
marcosvillarreal <marcosevillarreal@gmail.com>
2022-06-14 10:05:01 -0300

    m:transp

---
+++ Proyectos/transporte/Docs/Remitos_localidad.sql
@@ -0,0 +1,20 @@
+use elcoyote
+go
+
+select m.letra,m.talonario,m.numcomp,co.cabrevia,ca.total,ca.fecha,ca.idprovincia,pca.Nombre as ProvVenta
+,(cr.flete + cr.seguro + cr.comireembolso + cr.conexion) as flete,cr.iva
+
+
+
+
+from maopera as m
+left join comprobante as co on m.idcomproba = co.id
+inner join cabefac as ca on m.id = ca.idmaopera
+left join afecabefac as afe on ca.id = afe.idorigen and ca.idmaopera = afe.idmaoperao
+left join aferemito as aferto on ca.idmaopera = aferto.idmaoperac
+left join provincia as pca on ca.idprovincia = pca.id
+left join caberto as cr on afe.idafecta = cr.id or aferto.idmaoperar = cr.idmaopera
+
+where m.estado='0' and m.clasecomp in ('A','B','C','U') and (ca.fecha between '20220501' and '20220531')
+
+order by m.id
