8e3deb09e20176b87178e9197983dfbc4b973da7
marcosvillarreal <marcosevillarreal@gmail.com>
2022-08-05 11:04:16 -0300

    m:distribuidora

--- Proyectos/distribuidora/danone.bat
+++ Proyectos/distribuidora/danone.bat
@@ -4,4 +4,7 @@ cd prog*86*
 cd winscp
 winscp.com /ini=nul /script="c:\aplicaciones\tapia\danonesftp.txt"
 
+copy c:\aplicaciones\danone_exp\*.csv c:\aplicaciones\danone_exp\exp
+
 del c:\aplicaciones\danone_exp\* /Q
+
