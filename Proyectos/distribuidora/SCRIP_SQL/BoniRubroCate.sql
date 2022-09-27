fe4aeb49b129a39e20a6a9ecc39a16086d571cec
marcosvillarreal <marcosevillarreal@gmail.com>
2022-09-27 20:12:59 -0300

    m:distribuidora

---
+++ Proyectos/distribuidora/SCRIP_SQL/BoniRubroCate.sql
@@ -0,0 +1,24 @@
+USE [nuevasirena6]
+GO
+
+/****** Object:  Table [dbo].[bonirubro]    Script Date: 27/9/2022 19:49:27 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[bonirubrocate](
+	[id] [int] NOT NULL,
+	[idcatectacte] [int] NOT NULL,
+	[idrubro] [int] NOT NULL,
+	[bonifica] [numeric](6, 2) NOT NULL,
+	[fecha] [datetime] NOT NULL,
+ CONSTRAINT [PK_bonirubrocate] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
