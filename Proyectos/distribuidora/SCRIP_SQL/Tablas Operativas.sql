aa80bb4b4f91b087803fffd74dce989cbcd74093
marcosvillarreal <marcosevillarreal@gmail.com>
2022-10-05 10:39:02 -0300

    m:transporte

---
+++ Proyectos/distribuidora/SCRIP_SQL/Tablas Operativas.sql
@@ -0,0 +1,53 @@
+use almacen1810
+go
+
+delete from movstock
+delete from cuerfac
+delete from tablaimp
+delete from cabeasi
+delete from tablaasi
+delete from renmaope
+delete from maopera
+delete from cabefac
+delete from movcaja
+delete from movbcocar
+delete from movbcodeta
+delete from movtarjeta
+delete from producto
+delete from subproducto
+delete from existenc
+delete from cabeord
+delete from cuerord
+delete from cuervariord
+delete from cuercpra
+delete from cuervaricpra
+delete from cabecpra
+delete from ctacte
+delete from bonictacte
+delete from bonirubrocate
+delete from bonirubro
+delete from prodctacon
+delete from cbioprecio
+delete from antablaasi
+delete from antablaimp
+delete from ancabeasi
+delete from anmaopera
+delete from alertas
+delete from prodcodbarra
+delete from gestion
+delete from detanrocaja
+delete from fleteren
+delete from seguridad
+delete from ctactectacon
+delete from movctacte
+delete from afecabecpra
+delete from afecabefac
+delete from afeconcilia
+delete from afectacte
+delete from ctacteflete
+delete from ctacte_2
+delete from cotizadolar
+delete from padronafip
+delete from cabedeta
+execute actualizarid 1
+select * from keysid order by nextid desc
