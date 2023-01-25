897b5a3ea0ea5f1f3fc9d940449215bd3420c79f
marcosvillarreal <marcosevillarreal@gmail.com>
2023-01-25 18:22:25 -0300

    m:distribuidora

--- Proyectos/distribuidora/SCRIP_SQL/BoniRubroCate.sql
+++ Proyectos/distribuidora/SCRIP_SQL/BoniRubroCate.sql
@@ -1,4 +1,4 @@
-USE [nuevasirena6]
+USE libreria
 GO
 
 /****** Object:  Table [dbo].[bonirubro]    Script Date: 27/9/2022 19:49:27 ******/
@@ -14,10 +14,11 @@ CREATE TABLE [dbo].[bonirubrocate](
 	[idrubro] [int] NOT NULL,
 	[bonifica] [numeric](6, 2) NOT NULL,
 	[fecha] [datetime] NOT NULL,
+	[fechao] [datetime] NOT NULL
  CONSTRAINT [PK_bonirubrocate] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
 
