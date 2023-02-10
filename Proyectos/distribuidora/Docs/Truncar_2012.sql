9f873e854b4624285bec4ea421911c8ead2b336b
marcosvillarreal <marcosevillarreal@gmail.com>
2023-02-10 18:47:13 -0300

    m:distribuidora

--- Proyectos/distribuidora/Docs/Truncar_2012.sql
+++ Proyectos/distribuidora/Docs/Truncar_2012.sql
@@ -1,19 +1,19 @@
-use nuevasirena5
+use libreria
 go
-sp_helpdb nuevasirena5
+sp_helpdb libreria
 -- Antes de truncar el log cambiamos el modelo de recuperación a SIMPLE.
-ALTER DATABASE nuevasirena5
+ALTER DATABASE libreria
 SET RECOVERY SIMPLE;
 GO
 --Reducimos el log de transacciones a  1 MB.
-DBCC SHRINKFILE(nuevasirena5_log, 1);
+DBCC SHRINKFILE(libreria_log, 1);
 --Recucimos los archivos eliminados del principal
-DBCC SHRINKFILE(nuevasirena5, 1);
+DBCC SHRINKFILE(libreria, 1);
 GO
 -- Cambiamos nuevamente el modelo de recuperación a Completo.
-ALTER DATABASE nuevasirena5
+ALTER DATABASE libreria
 SET RECOVERY FULL;
 go
-sp_helpdb nuevasirena5
+sp_helpdb libreria
 GO
  
