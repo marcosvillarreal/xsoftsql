4d4c9209bfa7ee8e9d850c8601e87524c0539a7c
marcosvillarreal <marcosevillarreal@gmail.com>
2023-09-18 18:43:22 -0300

    varios

--- Proyectos/distribuidora/SCRIP_SQL/PM_estado.sql
+++ Proyectos/distribuidora/SCRIP_SQL/PM_estado.sql
@@ -1,15 +1,15 @@
 use pm
 go
---select * from empresa where id = 6
-select top 1 fecupdate,idempresa from vendedor --where idempresa = 6
+select * from empresa where id = 13
+select top 1 fecupdate,idempresa,'Vendedor' as tabla from vendedor --where idempresa = 6
 order by fecupdate desc
-select top 1 fecupdate,idempresa from secciones --where idempresa = 6 
+select top 1 fecupdate,idempresa,'Rubros' as tabla from secciones --where idempresa = 6 
 order by fecupdate desc
---select top 1 fecupdate,idempresa from articulos --where idempresa = 6 
---order by fecupdate desc
---select top 1 fecupdate,idempresa from codbarras --where idempresa = 3
---order by fecupdate desc
---select top 1 fecupdate,idempresa from clientes --where idempresa = 6 
---order by fecupdate desc
---select top 1 fecupdate,idempresa from movctacte --where idempresa = 13 
---order by fecupdate  desc
+select top 1 fecupdate,idempresa,'Articulos' as tabla from articulos --where idempresa = 6 
+order by fecupdate desc
+select top 1 fecupdate,idempresa,'Variedades' as tabla from codbarras --where idempresa = 3
+order by fecupdate desc
+select top 1 fecupdate,idempresa,'Clientes' as tabla from clientes --where idempresa = 6 
+order by fecupdate desc
+select top 1 fecupdate,idempresa,'Cta.Cte.' as tabla from movctacte --where idempresa = 13 
+order by fecupdate  desc
