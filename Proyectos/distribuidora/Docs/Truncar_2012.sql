80ba09705f26e2398e79a89a7c2b26a62f8b6276
marcosvillarreal <marcosevillarreal@gmail.com>
2023-11-21 16:26:46 -0300

    distribuidora

--- Proyectos/distribuidora/Docs/Truncar_2012.sql
+++ Proyectos/distribuidora/Docs/Truncar_2012.sql
@@ -1,19 +1,21 @@
-use libreria
+use distribuidorakm
 go
-sp_helpdb libreria
+sp_helpdb distribuidorakm
 -- Antes de truncar el log cambiamos el modelo de recuperación a SIMPLE.
-ALTER DATABASE libreria
+ALTER DATABASE distribuidorakm
 SET RECOVERY SIMPLE;
 GO
---Reducimos el log de transacciones a  1 MB.
-DBCC SHRINKFILE(libreria_log, 1);
+
 --Recucimos los archivos eliminados del principal
-DBCC SHRINKFILE(libreria, 1);
+DBCC SHRINKFILE(distribuidorakm, 1);
+--Reducimos el log de transacciones a  1 MB.
+DBCC SHRINKFILE(distribuidorakm_log, 1);
+
 GO
 -- Cambiamos nuevamente el modelo de recuperación a Completo.
-ALTER DATABASE libreria
+ALTER DATABASE distribuidorakm
 SET RECOVERY FULL;
 go
-sp_helpdb libreria
+sp_helpdb distribuidorakm
 GO
  
