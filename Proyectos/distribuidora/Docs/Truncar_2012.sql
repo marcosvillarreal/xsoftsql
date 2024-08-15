ec8c1eb35da52b99096fc5f1beee33ca50408ac7
marcosvillarreal <marcosevillarreal@gmail.com>
2024-08-15 08:28:39 -0300

    leon / distribuidora

--- Proyectos/distribuidora/Docs/Truncar_2012.sql
+++ Proyectos/distribuidora/Docs/Truncar_2012.sql
@@ -1,22 +1,22 @@
-use urquiza
+use campisi
 go
-sp_helpdb urquiza
+sp_helpdb campisi
 -- Antes de truncar el log cambiamos el modelo de recuperación a SIMPLE.
-ALTER DATABASE urquiza
+ALTER DATABASE campisi
 SET RECOVERY SIMPLE;
 GO
 
 --Recucimos los archivos eliminados del principal
-DBCC SHRINKFILE(urquiza, 1);
+DBCC SHRINKFILE(campisi, 1);
 --Reducimos el log de transacciones a  1 MB.
 go
-DBCC SHRINKFILE(urquiza_log, 1);
+DBCC SHRINKFILE(campisi_log, 1);
 
 GO
 -- Cambiamos nuevamente el modelo de recuperación a Completo.
-ALTER DATABASE urquiza
+ALTER DATABASE campisi
 SET RECOVERY FULL;
 go
-sp_helpdb urquiza
+sp_helpdb campisi
 GO
  
