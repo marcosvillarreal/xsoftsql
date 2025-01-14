4deb9b9858a42ad478149504301abff30209bba1
marcosvillarreal <marcosevillarreal@gmail.com>
2025-01-14 16:52:35 -0300

    leon

--- Proyectos/consultorio/SCRIP_SQL/Truncar_2012.sql
+++ Proyectos/consultorio/SCRIP_SQL/Truncar_2012.sql
@@ -1,24 +1,24 @@
-use meridiem
+use corralon_ant
 go
 execute actualizarid 1
 go
-sp_helpdb meridiem
+sp_helpdb corralon_ant
 -- Antes de truncar el log cambiamos el modelo de recuperación a SIMPLE.
-ALTER DATABASE meridiem
+ALTER DATABASE corralon_ant
 SET RECOVERY SIMPLE;
 GO
 
 --Recucimos los archivos eliminados del principal
-DBCC SHRINKFILE(meridiem, 1);
+DBCC SHRINKFILE(corralon_ant, 1);
 --Reducimos el log de transacciones a  1 MB.
 go
-DBCC SHRINKFILE(meridiem_log, 1);
+DBCC SHRINKFILE(corralon_ant_log, 1);
 
 GO
 -- Cambiamos nuevamente el modelo de recuperación a Completo.
-ALTER DATABASE meridiem
+ALTER DATABASE corralon_ant
 SET RECOVERY FULL;
 go
-sp_helpdb meridiem
+sp_helpdb corralon_ant
 GO
- execute sp_backupdatabase 'meridiem','F'
+ execute sp_backupdatabase 'corralon_ant','F'
