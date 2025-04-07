c9ef8a9444acfeb1f97f3afe733659f4b6bd20d5
marcosvillarreal <marcosevillarreal@gmail.com>
2025-04-07 11:28:18 -0300

    mildelicias

---
+++ Proyectos/distribuidora/SCRIP_SQL/Tapia/danone.sql
@@ -0,0 +1,26 @@
+use tapia
+go
+select ca.ctacte,ca.cnombre
+,rtrim(convert(char(8),cu.codigo)) + (case when isnull(cv.idsubarti,0) = 0 then ''
+else ( case when len(ltrim(sp.codartprovee))=0 then 
+	right('000'+rtrim(ltrim(convert(char(8),sp.subnumero))),3) 
+	else ltrim(sp.codartprovee)  end) end)
+,rtrim(cu.nombre) + (case when isnull(cv.idsubarti,0) = 0 then '' else '-'+sp.nombre end)
+,sum( (case when isnull(cv.idsubarti,0)=0 then cu.cantidad else cv.cantidad end) * p.peso) as vtapeso
+,avg(cu.preunitasiva) as preunita
+,avg(cu.despor) as bonif
+,ca.fecha
+from maopera as m
+inner join cabefac as ca on m.id = ca.idmaopera
+inner join cuerfac as cu on ca.id = cu.idcabeza
+left join cuervari as cv on cu.id = cv.idcuerfac
+left join ctacte on ca.idctacte = ctacte.id
+left join canalvta as cn on ctacte.idcanalvta = cn.id
+left join localidad as lo on ctacte.idlocalidad = lo.id
+inner join producto as p on cu.idarticulo = p.id
+left join subproducto as sp on cv.idsubarti = sp.id
+where m.estado=0 and ca.fecha between '20250201' and '20250228' 
+and p.idctacte in (1100001480,1100001481)
+and m.clasecomp in ('A')
+group by ca.ctacte,ca.cnombre,ca.cdireccion,lo.nombre,cn.nombre ,cu.codigo,cu.nombre,sp.subnumero,cv.idsubarti,sp.nombre
+,ca.fecha,sp.codartprovee
