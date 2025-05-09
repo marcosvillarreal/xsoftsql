eab496ee9850f585f152b6434aed3faa83f4e5a8
marcosvillarreal <marcosevillarreal@gmail.com>
2025-05-09 11:36:02 -0300

    distribuidora

--- Proyectos/distribuidora/Docs/Truncar_2012.sql
+++ Proyectos/distribuidora/Docs/Truncar_2012.sql
@@ -1,24 +1,24 @@
-use corralon_ant
+use distmuller
 go
-execute actualizarid 1
+--execute actualizarid 1
 go
-sp_helpdb corralon_ant
+sp_helpdb distmuller
 -- Antes de truncar el log cambiamos el modelo de recuperación a SIMPLE.
-ALTER DATABASE corralon_ant
+ALTER DATABASE distmuller
 SET RECOVERY SIMPLE;
 GO
 
 --Recucimos los archivos eliminados del principal
-DBCC SHRINKFILE(corralon_ant, 1);
+DBCC SHRINKFILE(distmuller, 1);
 --Reducimos el log de transacciones a  1 MB.
 go
-DBCC SHRINKFILE(corralon_ant_log, 1);
+DBCC SHRINKFILE(distmuller_log, 1);
 
 GO
 -- Cambiamos nuevamente el modelo de recuperación a Completo.
-ALTER DATABASE corralon_ant
+ALTER DATABASE distmuller
 SET RECOVERY FULL;
 go
-sp_helpdb corralon_ant
+sp_helpdb distmuller
 GO
- execute sp_backupdatabase 'corralon_ant','F'
+ --execute sp_backupdatabase 'distmuller','F'
