e8f1e4c13ec011abbd0dff30d62f09c22b89709c
marcosvillarreal <marcosevillarreal@gmail.com>
2025-05-23 17:25:53 -0300

    leon

--- Proyectos/leon/danone.bat
+++ Proyectos/leon/danone.bat
@@ -4,4 +4,5 @@ c:
 cd prog*86*
 cd winscp
 winscp.com /ini=nul /script="J:\XSOFTSQL\PROYECTOS\LEON\danonesftp.txt"
-del C:\GMSOLUTIONS\LEON\danone_exp\* /Q
+copy C:\GMSOLUTIONS\LEON\danone_exp\*.csv C:\GMSOLUTIONS\LEON\danone_exp\hist 
+del C:\GMSOLUTIONS\LEON\danone_exp\*.csv /Q
