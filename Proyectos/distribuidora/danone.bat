954cf07c6bca21daf273e313554e18cca2248f15
marcosvillarreal <marcosevillarreal@gmail.com>
2025-06-24 09:21:24 -0300

    distribuidora
    exportador

--- Proyectos/distribuidora/danone.bat
+++ Proyectos/distribuidora/danone.bat
@@ -3,6 +3,6 @@ cd\
 c:
 cd prog*86*
 cd winscp
-winscp.com /ini=nul /script="J:\XSOFTSQL\PROYECTOS\DISTRIBUIDORA\danonesftp.txt"
+winscp.com /ini=nul /script="J:\XSOFTSQL\PROYECTOS\DISTRIBUIDORA\nextbynftp.txt"
 copy J:\APLICACIONES\GARRONE\*.csv J:\APLICACIONES\GARRONE\hist 
 del J:\APLICACIONES\GARRONE\*.csv /Q
