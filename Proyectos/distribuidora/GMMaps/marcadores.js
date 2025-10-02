a732ae880051f7cbbbd505aa599495635d835df2
marcosvillarreal <marcosevillarreal@gmail.com>
2025-10-02 12:06:42 -0300

    distribuidora

--- Proyectos/distribuidora/GMMaps/marcadores.js
+++ Proyectos/distribuidora/GMMaps/marcadores.js
@@ -29,176 +29,306 @@ icons = {
 	black: createColorIcon('black')
 };
 
-const marcadores = [																
+const marcadores = [																													
 	
 	{
-		lat: -39.250789,
-		lng: -62.611782,
+		lat: -38.7446507551018,
+		lng: -62.1894231779108,
 		icon: 'green',
-		popup: 'CALLE 6 1576',
-		alt: '195',
-		nombre: 'RESCHKE OSCAR DANIEL'
+		popup: 'INGENIERO LUNA 2361  BARRIO',
+		alt: '2797',
+		nombre: 'GONZALEZ SARA'
 	}
 	,
 	{
-		lat: -39.259055,
-		lng: -62.615444,
+		lat: -38.7199204870122,
+		lng: -62.2007585220892,
 		icon: 'green',
-		popup: 'JULIO A COUSTE 745',
-		alt: '227',
-		nombre: 'FANG KAI'
+		popup: 'LUIS AGOTE 3171',
+		alt: '1175',
+		nombre: 'MARDONES NOEMI GRACIELA'
 	}
 	,
 	{
-		lat: -39.495092,
-		lng: -62.692337,
+		lat: -38.729701635509,
+		lng: -62.2219596423237,
 		icon: 'green',
-		popup: '16   422',
-		alt: '311',
-		nombre: 'VERON SERGIO ALEJANDRO'
+		popup: 'DORBIGNY 2718',
+		alt: '2146',
+		nombre: 'STELE CLAUDIA PATRICIA'
 	}
 	,
 	{
-		lat: -39.49517,
-		lng: -62.68425,
+		lat: -38.7146180874,
+		lng: -62.1887942017722,
 		icon: 'green',
-		popup: '19 Y 30 0',
-		alt: '926',
-		nombre: 'BAKOS MAXIMILIANO'
+		popup: 'QUINQUELA MARTIN 4144HARDEEN',
+		alt: '49',
+		nombre: 'SAVOF NATALIA'
 	}
 	,
 	{
-		lat: -39.260379,
-		lng: -62.614460,
+		lat: -38.72319,
+		lng: -62.19881,
 		icon: 'green',
-		popup: 'SARMIENTO 1410',
-		alt: '992',
-		nombre: 'RESIDENCIA EL PINAR DE BURATO SRL'
+		popup: 'ROBERTO CLEGG 3338',
+		alt: '226',
+		nombre: 'PERFECTIVA SANTOS'
 	}
 	,
 	{
-		lat: -39.502910,
-		lng: -62.683985,
+		lat: -38.7295586046741,
+		lng: -62.2180488067465,
 		icon: 'green',
-		popup: 'CALLE 26  1162',
-		alt: '1019',
-		nombre: 'LARZABAL DELIA HAYDEE'
+		popup: 'GUARDIA VIEJA Y ROJAS',
+		alt: '36',
+		nombre: 'MARTINEZ MARISOL'
 	}
 	,
 	{
-		lat: -39.499949,
-		lng: -62.680723,
+		lat: -38.730232,
+		lng: -62.218521,
 		icon: 'green',
-		popup: 'CALLE 32  244',
-		alt: '1268',
-		nombre: 'TITO JESICA PAMELA'
+		popup: 'GUARDIA VIEJA 1821',
+		alt: '1665',
+		nombre: 'FANTAGUSI PATRICIA'
 	}
 	,
 	{
-		lat: -39.37703,
-		lng: -62.6486,
+		lat: -38.7518419429706,
+		lng: -62.1772842067465,
 		icon: 'green',
-		popup: 'AMEDIA DEHENEN Y CALLE 12',
-		alt: '1280',
-		nombre: 'LIN JIANHONG'
+		popup: 'CONSTANCIO VIGIL 1445',
+		alt: '2757',
+		nombre: 'DURAN MARCOS DANIEL'
 	}
 	,
 	{
-		lat: -39.261653,
-		lng: -62.619503,
+		lat: -38.74331,
+		lng: -62.16455,
 		icon: 'green',
-		popup: 'SANTIAGO BURATOVICH 1756',
-		alt: '1321',
-		nombre: 'ALZORRIZ ANDRES ANTONIO'
+		popup: 'TTE MAYO 3625',
+		alt: '2771',
+		nombre: 'SAGRIPANTI GABRIELA EDITH'
 	}
 	,
 	{
-		lat: -39.262633,
-		lng: -62.620249,
+		lat: -38.7462102270709,
+		lng: -62.1898763509248,
 		icon: 'green',
-		popup: 'SAN MARTIN 766',
-		alt: '1327',
-		nombre: 'HERRADA GIL ADRIAN NICOLAS'
+		popup: 'LUCIO MANSILLA 2235',
+		alt: '2775',
+		nombre: 'MONTENEGRO MARIANA SOLEDAD'
 	}
 	,
 	{
-		lat: -39.260803,
-		lng: -62.607636,
+		lat: -38.7490517672229,
+		lng: -62.1931233797605,
 		icon: 'green',
-		popup: 'RENE FAVALORO 756',
-		alt: '1329',
-		nombre: 'FUNES ANA MARIA'
+		popup: 'MANSILLA 1867',
+		alt: '1079',
+		nombre: 'MATEO JORGE DANIE'
 	}
 	,
 	{
-		lat: -39.50464,
-		lng: -62.68194,
+		lat: -38.7366654706984,
+		lng: -62.2269195644178,
 		icon: 'green',
-		popup: 'CALLE 28 N 103',
-		alt: '2081',
-		nombre: 'CHEN QUIN'
+		popup: 'GUARDIA VIEJA Y NEWTON',
+		alt: '1085',
+		nombre: 'MERCADITO LA CHOLA'
 	}
 	,
 	{
-		lat: -39.264692,
-		lng: -62.615710,
+		lat: -38.717112066038,
+		lng: -62.1940632779108,
 		icon: 'green',
-		popup: 'REPUBLICA ARGENTINA 562',
-		alt: '2121',
-		nombre: 'CONDORI ABIGAIL'
+		popup: 'MARTIN A MALHARRO 3328',
+		alt: '133',
+		nombre: 'ECHARRI SEBASTIAN HORACIO'
 	}
 	,
 	{
-		lat: -39.494713,
-		lng: -62.690331,
+		lat: -38.7124309162762,
+		lng: -62.1940299435559,
 		icon: 'green',
-		popup: 'CALLE 22 N163',
-		alt: '2161',
-		nombre: 'PRODUCTOS Y SERVICIOS MISKY MAYO SRL EN'
+		popup: 'SALINAS 3814',
+		alt: '823',
+		nombre: 'SAVOFF MAURO DEMETRIO'
 	}
 	,
 	{
-		lat: -39.50381,
-		lng: -62.68234,
+		lat: -38.7392921088297,
+		lng: -62.2136199644178,
 		icon: 'green',
-		popup: 'CALLE 28 Y 3',
-		alt: '2253',
-		nombre: 'SARTISON NICOLAS MARTIN'
+		popup: 'SAN JOSE 1260',
+		alt: '2594',
+		nombre: 'ZHENG XUEQIN'
 	}
 	,
 	{
-		lat: -39.376025,
-		lng: -62.648389,
+		lat: -38.7384382008263,
+		lng: -62.2293984220892,
 		icon: 'green',
-		popup: 'CALLE 5   399',
-		alt: '1335',
-		nombre: 'GARAT MATIAS URIEL'
+		popup: 'GUARDIA VIEJA 554',
+		alt: '141',
+		nombre: 'GARRIDO HIDALGO EDUARDO ENRIQUE'
+	}
+	,
+	{
+		lat: -38.7383510377534,
+		lng: -62.2177226067465,
+		icon: 'green',
+		popup: 'RINCON 3435',
+		alt: '2766',
+		nombre: 'SALGADO PEREZ ALEXANDER JESUS'
+	}
+	,
+	{
+		lat: -38.7484528003969,
+		lng: -62.1814511644178,
+		icon: 'green',
+		popup: 'MARTIN CORONADO 5433',
+		alt: '2798',
+		nombre: 'SEPULVEDA MABEL'
+	}
+	,
+	{
+		lat: -38.7134043529495,
+		lng: -62.1946649220892,
+		icon: 'green',
+		popup: 'INDIADA 4035',
+		alt: '1505',
+		nombre: 'FUENTES BERTA SILVANA (SUCURSAL)'
+	}
+	,
+	{
+		lat: -38.7324793294383,
+		lng: -62.2231330932535,
+		icon: 'green',
+		popup: 'LAINEZ 2840',
+		alt: '981',
+		nombre: 'CHEN JIANFA'
+	}
+	,
+	{
+		lat: -38.7387003769744,
+		lng: -62.2241723139313,
+		icon: 'green',
+		popup: 'INDIADA 700',
+		alt: '929',
+		nombre: 'LI HAIXIA'
+	}
+	,
+	{
+		lat: -38.74901,
+		lng: -62.19397,
+		icon: 'green',
+		popup: 'BAIGORRIA 4638',
+		alt: '1759',
+		nombre: 'FUENTES VERONICA ANDREA'
+	}
+	,
+	{
+		lat: -38.749930,
+		lng: -62.194112,
+		icon: 'green',
+		popup: 'LUCIO MANSILLA 2193',
+		alt: '2776',
+		nombre: 'VEDOVALDI SILVINA ALEJANDRA'
+	}
+	,
+	{
+		lat: -38.683669,
+		lng: -62.293461,
+		icon: 'green',
+		popup: 'REPUBLICA SIRIA 3301',
+		alt: '2355',
+		nombre: 'SACOMANI MARIANO NICOLAS'
+	}
+	,
+	{
+		lat: -38.666957,
+		lng: -62.266102,
+		icon: 'green',
+		popup: 'VERA 3025',
+		alt: '212',
+		nombre: 'LLEIFUL CARLOS DARIO'
+	}
+	,
+	{
+		lat: -38.206600,
+		lng: -61.771590,
+		icon: 'green',
+		popup: 'BELGRANO 202',
+		alt: '2803',
+		nombre: 'DUBE ARIEL ALEJANDRO'
+	}
+	,
+	{
+		lat: -38.781871,
+		lng: -62.267963,
+		icon: 'green',
+		popup: 'AV SAN MARTIN 3489',
+		alt: '1667',
+		nombre: 'VEGA MARCELO MARTIN'
+	}
+	,
+	{
+		lat: -38.768586,
+		lng: -62.275307,
+		icon: 'green',
+		popup: 'MAESTRO PICCIOLI 3391',
+		alt: '1607',
+		nombre: 'BOBADILLA DANIEL'
+	}
+	,
+	{
+		lat: -38.783548,
+		lng: -62.265757,
+		icon: 'green',
+		popup: 'SAN MARTIN 3671',
+		alt: '892',
+		nombre: 'KE WUJIN'
 	}]
 
 
 const recorridos = {		
 		ruta1:{
-			nombre: "PEDRO_LURO                                        ",
+			nombre: "MARTIN                                            ",
 			color: "#e74c3c",
 			peso:4,
 			puntos: [
 				
-[-39.250789,-62.611782],
-[-39.259055,-62.615444],
-[-39.495092,-62.692337],
-[-39.49517,-62.68425],
-[-39.260379,-62.614460],
-[-39.502910,-62.683985],
-[-39.499949,-62.680723],
-[-39.37703,-62.6486],
-[-39.261653,-62.619503],
-[-39.262633,-62.620249],
-[-39.260803,-62.607636],
-[-39.50464,-62.68194],
-[-39.264692,-62.615710],
-[-39.494713,-62.690331],
-[-39.50381,-62.68234],
-[-39.376025,-62.648389]
+[-38.7446507551018,-62.1894231779108],
+[-38.7199204870122,-62.2007585220892],
+[-38.729701635509,-62.2219596423237],
+[-38.7146180874,-62.1887942017722],
+[-38.72319,-62.19881],
+[-38.7295586046741,-62.2180488067465],
+[-38.730232,-62.218521],
+[-38.7518419429706,-62.1772842067465],
+[-38.74331,-62.16455],
+[-38.7462102270709,-62.1898763509248],
+[-38.7490517672229,-62.1931233797605],
+[-38.7366654706984,-62.2269195644178],
+[-38.717112066038,-62.1940632779108],
+[-38.7124309162762,-62.1940299435559],
+[-38.7392921088297,-62.2136199644178],
+[-38.7384382008263,-62.2293984220892],
+[-38.7383510377534,-62.2177226067465],
+[-38.7484528003969,-62.1814511644178],
+[-38.7134043529495,-62.1946649220892],
+[-38.7324793294383,-62.2231330932535],
+[-38.7387003769744,-62.2241723139313],
+[-38.74901,-62.19397],
+[-38.749930,-62.194112],
+[-38.683669,-62.293461],
+[-38.666957,-62.266102],
+[-38.206600,-61.771590],
+[-38.781871,-62.267963],
+[-38.768586,-62.275307],
+[-38.783548,-62.265757]
 			]
 		}}
 
