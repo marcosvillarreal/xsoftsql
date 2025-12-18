6dcc0342d61dbab51420b84bde3a7ee43574179f
Marcos Villarreal <marcosevillarreal@gmail.com>
2025-12-18 09:55:18 -0300

    DISTRIBUIDORA

--- Proyectos/distribuidora/Docs/Truncar_2012.sql
+++ Proyectos/distribuidora/Docs/Truncar_2012.sql
@@ -1,24 +1,24 @@
-use distmuller
+use master
 go
 --execute actualizarid 1
 go
-sp_helpdb distmuller
+sp_helpdb marucafiam
 -- Antes de truncar el log cambiamos el modelo de recuperación a SIMPLE.
-ALTER DATABASE distmuller
+ALTER DATABASE marucafiam
 SET RECOVERY SIMPLE;
 GO
 
 --Recucimos los archivos eliminados del principal
-DBCC SHRINKFILE(distmuller, 1);
+DBCC SHRINKFILE(marucafiam, 1);
 --Reducimos el log de transacciones a  1 MB.
 go
-DBCC SHRINKFILE(distmuller_log, 1);
+DBCC SHRINKFILE(marucafiam_log, 1);
 
 GO
 -- Cambiamos nuevamente el modelo de recuperación a Completo.
-ALTER DATABASE distmuller
+ALTER DATABASE marucafiam
 SET RECOVERY FULL;
 go
-sp_helpdb distmuller
+sp_helpdb marucafiam
 GO
- execute sp_backupdatabase 'distmuller','F'
+ execute sp_backupdatabase 'marucafiam','F'
