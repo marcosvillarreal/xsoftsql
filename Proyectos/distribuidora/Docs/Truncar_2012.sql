e7b5e76b498c2e32a6af81c403621b6df2d7650e
Marcos Villarreal <marcosevillarreal@gmail.com>
2026-01-22 07:28:41 -0300

    comercial

--- Proyectos/distribuidora/Docs/Truncar_2012.sql
+++ Proyectos/distribuidora/Docs/Truncar_2012.sql
@@ -1,24 +1,24 @@
-use marucafiam
+use teomayorista04
 go
 --execute actualizarid 1
 go
-sp_helpdb marucafiam
+sp_helpdb teomayorista04
 -- Antes de truncar el log cambiamos el modelo de recuperación a SIMPLE.
-ALTER DATABASE marucafiam
+ALTER DATABASE teomayorista04
 SET RECOVERY SIMPLE;
 GO
 
 --Recucimos los archivos eliminados del principal
-DBCC SHRINKFILE(marucafiam, 1);
+DBCC SHRINKFILE(teomayorista04, 1);
 --Reducimos el log de transacciones a  1 MB.
 go
-DBCC SHRINKFILE(marucafiam_log, 1);
+DBCC SHRINKFILE(teomayorista04_log, 1);
 
 GO
 -- Cambiamos nuevamente el modelo de recuperación a Completo.
-ALTER DATABASE marucafiam
+ALTER DATABASE teomayorista04
 SET RECOVERY FULL;
 go
-sp_helpdb marucafiam
+sp_helpdb teomayorista04
 GO
- execute sp_backupdatabase 'marucafiam','F'
+ execute sp_backupdatabase 'teomayorista04','F'
