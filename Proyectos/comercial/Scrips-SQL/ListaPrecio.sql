746d8a22d8c0ab27453048bb7d50b6f470cdff93
marcosvillarreal <marcosevillarreal@gmail.com>
2026-07-15 19:30:31 -0300

    leon

--- Proyectos/comercial/Scrips-SQL/ListaPrecio.sql
+++ Proyectos/comercial/Scrips-SQL/ListaPrecio.sql
@@ -1,7 +1,8 @@
+go
 
 GO
 
-/****** Object:  Table [dbo].[listaprecio]    Script Date: 26/11/2025 18:33:44 ******/
+/****** Object:  Table [dbo].[listaprecio]    Script Date: 15/7/2026 10:55:00 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -13,12 +14,13 @@ CREATE TABLE [dbo].[listaprecio](
 	[numero] [int] NOT NULL,
 	[nombre] [nvarchar](50) NOT NULL,
 	[margen] [numeric](6, 3) NOT NULL,
-	[abrevia] [char](2) NOT NULL,
+	[abrevia] [char](2) NULL,
  CONSTRAINT [PK_listaprecio] PRIMARY KEY CLUSTERED 
 (
 	[id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
 
 
--- Proyectos/leon/danone.bat
+++ Proyectos/leon/danone.bat
@@ -4,5 +4,5 @@ c:
 cd prog*86*
 cd winscp
 winscp.com /ini=nul /script="J:\XSOFTSQL\PROYECTOS\LEON\danonesftp.txt"
-copy C:\GMSOLUTIONS\LEON\danone_exp\*.csv C:\GMSOLUTIONS\LEON\danone_exp\hist 
+copy C:\GMSOLUTIONS\LEON\danone_exp\*.csv C:\GMSOLUTIONS\LEON\danone_exp\hist\
 del C:\GMSOLUTIONS\LEON\danone_exp\*.csv /Q
